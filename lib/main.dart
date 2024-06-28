bd5b9a4591e44bffa9fe133ac4cd6b54ddef3708
irsen <irsen.kan@bk.ru>
2024-06-28 14:44:40 +0900

    add data and show list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/widgets/grocery_list.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Groceries',
+      theme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 42, 51, 59),
         ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
+      home: const GroceryList(),
     );
   }
 }
---
+++ lib/models/grocery_item.dart
@@ -0,0 +1,16 @@
+
+import 'package:to_do_list/models/categories.dart';
+
+class GroceryItem {
+  const GroceryItem({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.category,
+  });
+
+  final String id;
+  final String name;
+  final int quantity;
+  final Category category;
+}
